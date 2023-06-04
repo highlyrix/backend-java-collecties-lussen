d5383dc9ab9eddb7a1a2faaab339f5f06926c2e6
Rick Jonkman <highlyrix@gmail.com>
2023-06-04 14:04:03 +0200

    Translator en bonus

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,6 +1,7 @@
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Bonus {
 
@@ -10,39 +11,57 @@ public class Bonus {
         // In de code staan op verschillende plekken comments tussen "/*...*/" als hint wat je moet doen
 
 
+
         HashSet<Integer> secretnumber = randomnumbergenerator();
         String stringnumber = setToStringConverter(secretnumber);
         System.out.println(stringnumber);
-        feedback(/*vul hier het juiste argument in*/);
+        feedback("1234");
 
     }
 
-    public static void/*moet dit returntype "void" zijn of wat anders?*/ randomnumbergenerator(/*Heeft deze methode nog parameter(s) nodig?*/){
-         /*
-        Vul hier de body van de methode in.
+    public static HashSet<Integer> randomnumbergenerator(){
+        Random random = new Random();
 
-        Stappenplan:
-        - Maak een nieuwe variabele van type Random. (Tip: Zoek op internet hoe je Random kunt gebruiken)
-        - Maak een nieuwe variabele van type HashSet.
-        - Schrijf een while-loop om 4 random nummers aan de hashset toe te voegen
-        - return de hashset
-         */
+        HashSet<Integer> hashNumber = new HashSet<>();
+
+        while (hashNumber.size() < 4) {
+            int randomNumber = random.nextInt();
+            hashNumber.add(randomNumber);
+        }
+        return hashNumber;
+
+
+//        Stappenplan:
+//        - Maak een nieuwe variabele van type Random. (Tip: Zoek op internet hoe je Random kunt gebruiken)
+//        - Maak een nieuwe variabele van type HashSet.
+//        - Schrijf een while-loop om 4 random nummers aan de hashset toe te voegen
+//        - return de hashset
+
     }
 
-    public static void/*moet dit returntype "void" zijn of wat anders?*/ setToStringConverter(/*Heeft deze methode nog parameter(s) nodig?*/){
-        /*
-        Vul hier de body van de methode in.
+    public static String setToStringConverter(HashSet<Integer> hashNumber){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int number : hashNumber
+        ) { stringBuilder.append(number);
 
-        Stappenplan:
-        - Maak als eerst een String variabele met als waarde een lege String. (of gebruik een StringBuilder)
-        - Schrijf vervolgens een for-loop om de items in de hashset een voor een aan de String variabele toe te voegen.
-        - Return de (gevulde) String variabele
-         */
+        }
+        return stringBuilder.toString();
     }
 
 
+//
+//        Vul hier de body van de methode in.
+//
+//        Stappenplan:
+//        - Maak als eerst een String variabele met als waarde een lege String. (of gebruik een StringBuilder)
+//        - Schrijf vervolgens een for-loop om de items in de hashset een voor een aan de String variabele toe te voegen.
+//        - Return de (gevulde) String variabele
+//
 
-    public static void/*moet dit "void" zijn of wat anders?*/ feedback(String stringnumber) {
+
+
+
+    public static void feedback(String stringnumber) {
         Scanner scanner = new Scanner(System.in);
         StringBuilder feedback = new StringBuilder();
         System.out.println("+ = juiste nummer op de juiste plek, O = juiste nummer verkeerde plek, X = verkeerde nummer");
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,37 @@
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        boolean play = true;
+        String ongeldig = "ongeldige invoer";
+        Scanner scanner = new Scanner(System.in);
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
+        Integer[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String[] alphabetic = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"};
 
+        Translator translator = new Translator(numeric, alphabetic);
+
+        while (play == true) {
+            System.out.println("Type 'x' om te stoppen \nType 'v' om te vertalen.");
+            String input = scanner.nextLine();
+            if (Objects.equals(input, "x")) {
+                play = false;
+            } else if (Objects.equals(input, "v")) {
+                System.out.println("Type een cijfer in van 0 t/m 9");
+                int number = scanner.nextInt();
+                scanner.nextLine();
+                if (number < 10) {
+                    String result = translator.translate(number);
+                    System.out.println("De vertaling van " + number + " is " + result);
+                } else {
+                    System.out.println(ongeldig);
+                }
+            } else {
+                System.out.println(ongeldig);
+            }
+        }
 
     }
 }
